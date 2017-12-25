9df73535da8848b9be6b8b50f6572e76a679270d
ericvyc <eric_vilar2007@hotmail.com>
2017-12-25 17:48:17 -0500

    Validacao de dados e tratamento de erros

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -24,20 +24,20 @@ module.exports = function(app){
   });
 
   app.get('/produtos/form', function(req, res){
-    res.render('produtos/form');
+    res.render('produtos/form', {errosValidacao : {}});
   });
 
   app.post('/produtos', function(req, res){
 
     var produto = req.body;
 
-    var validTitulo = req.assert('titulo', 'Titulo e obrigatorio');
-    validTitulo.notEmpty();
+    req.assert('titulo', 'Titulo e obrigatorio').notEmpty();
+    req.assert('preco', 'Formato invalido').isFloat();
 
     var erros = req.validationErrors();
 
     if(erros){
-      res.render('produtos/form');
+      res.render('produtos/form', { errosValidacao : erros });
       return;
     }
 
