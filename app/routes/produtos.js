33d37d201b45e47eae64d382baecbaacff0439a8
ericvyc <eric_vilar2007@hotmail.com>
2017-12-25 12:46:22 -0500

    Arquitetura do codigo

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -3,12 +3,17 @@ module.exports = function(app){
   app.get('/produtos', function(req, res){
 
     var connection = app.infra.connectionFactory();
+    var produtosBanco = app.infra.produtosBanco;
 
-    connection.query('select * from livros', function(err, results){
-      res.render('produtos/lista',{ lista : results })
+    produtosBanco.lista(connection, function(err, results){
+      res.render('produtos/lista',{ lista : results });
     });
 
     connection.end();
+  });
+
+  app.get('produtos/detalhe', function () {
+    produtosBanco.carrega(connection, id, callback);
   });
 
 }
