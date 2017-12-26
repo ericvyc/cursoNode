9089e44ceeb15e946f8dcd87fe1951868760194d
ericvyc <eric_vilar2007@hotmail.com>
2017-12-25 20:46:44 -0500

    Testes com supertest.

--- test/produtos.js
+++ test/produtos.js
@@ -1,24 +1,14 @@
-var http = require('http');
-var assert = require('assert');
+var express = require('../config/express')();
+var request = require('supertest')(express);
 
 describe('#ProdutosController', function() {
   it('#listagem json', function(done) {
 
-    var config = {
-      hostname : 'localhost',
-      port : 3000,
-      path : '/produtos',
-      headers : {
-        'Accept' : 'application/json'
-      }
-    };
+    request.get("/produtos")
+    .set('Accept','application/json')
+    .expect('Content-type',/json/)
+    .expect(200, done);
 
-    http.get(config, function(res) {
+  });
 
-      assert.equal(res.statusCode, 200);
-      assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
-      done();
-
-    });
-  })
 });
