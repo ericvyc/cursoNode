7798f4a868e049b3c592b4ffb89b3f0b4311b54b
ericvyc <eric_vilar2007@hotmail.com>
2017-12-30 12:27:54 -0500

    test

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -2,32 +2,32 @@ var mysql = require('mysql');
 
 var connection = function () {
 
-  if(!process.env.NODE_ENV) {
+  // if(!process.env.NODE_ENV) {
     return mysql.createConnection({
       host : 'cursonode.mysql.dbaas.com.br',
       user : 'cursonode',
       password : 'motopartes2018',
       database : 'cursonode'
     });
-  }
+  // }
 
-  if(!process.env.NODE_ENV == 'test') {
-    return mysql.createConnection({
-      host : 'cursonode.mysql.dbaas.com.br',
-      user : 'cursonode',
-      password : 'motopartes2018',
-      database : 'cursonode_test'
-    });
-  }
-
-  if(!process.env.NODE_ENV == 'production') {
-    return mysql.createConnection({
-      host : 'cursonode.mysql.dbaas.com.br',
-      user : 'cursonode',
-      password : 'motopartes2018',
-      database : 'cursonode'
-    });
-  }
+  // if(!process.env.NODE_ENV == 'test') {
+  //   return mysql.createConnection({
+  //     host : 'cursonode.mysql.dbaas.com.br',
+  //     user : 'cursonode',
+  //     password : 'motopartes2018',
+  //     database : 'cursonode_test'
+  //   });
+  // }
+  //
+  // if(!process.env.NODE_ENV == 'production') {
+  //   return mysql.createConnection({
+  //     host : 'cursonode.mysql.dbaas.com.br',
+  //     user : 'cursonode',
+  //     password : 'motopartes2018',
+  //     database : 'cursonode'
+  //   });
+  // }
 
 }
 
